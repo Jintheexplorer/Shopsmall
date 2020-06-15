6a2d3b59c0f587c88aff0cf71227eaa73feabee6
Jintheexplorer <jinwan.hyun@gmail.com>
2020-06-15 12:58:19 +0000

    initial update

--- main.py
+++ main.py
@@ -1,7 +1,13 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, jsonify
 from random import choice
+import request
+from bs4 import BeautifulSoup
 
-web_site = Flask(__name__)
+from pymongo import MongoClient #pymongo 임포트하기
+client = MongoClient('mongodb://jin:jin47657306@ds011459.mlab.com:11459/heroku_xmp8k4vm?retryWrites=false', 27017)
+db = client['heroku_xmp8k4vm']
+
+app = Flask(__name__)
 
 number_list = [
 	100, 101, 200, 201, 202, 204, 206, 207, 300, 301, 302, 303, 304, 305, 307, 400, 401, 402, 403, 404, 405, 406, 408, 409, 410, 411, 412, 413, 414, 415,
@@ -9,23 +15,21 @@ number_list = [
 	429, 431, 444, 450, 451, 500, 502, 503, 504, 506, 507, 508, 509, 510, 511, 599
 ]
 
-@web_site.route('/')
+@app.route('/')
 def index():
 	return render_template('index.html')
 
-@web_site.route('/user/', defaults={'username': None})
-@web_site.route('/user/<username>')
-def generate_user(username):
-	if not username:
-		username = request.args.get('username')
+@app.route('/topten', methods=['GET'])
+def displaytop10():
+  #네이버쇼핑 검색 상위 10개 가져오기 (스크래핑)
+  #썸네일, 텍스트, 가격
+  #
+  return jsonify({"result":[{"img":"a"},{"img":"b"}]})
 
-	if not username:
-		return 'Sorry error something, malformed request.'
+@app.route('/clicktrend', methods=['GET'])
+def clicktrend():
+  pass
 
-	return render_template('personal_user.html', user=username)
+if __name__=="__main__":
 
-@web_site.route('/page')
-def random_page():
-  return render_template('page.html', code=choice(number_list))
-
-web_site.run(host='0.0.0.0', port=8080)
+  app.run(host='0.0.0.0', port=8080)
